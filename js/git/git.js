8d325e6cc4141aab04e91a5b4bd538a6207d4136
Greg Fedirko <gjfedirko@gmail.com>
2014-11-10 20:31:06 -0800

    git checkout working

--- js/git/git.js
+++ js/git/git.js
@@ -7,11 +7,13 @@ var _status = require('./functions/git.status.js');
 var _add = require('./functions/git.add.js');
 var _commit = require('./functions/git.commit.js');
 var _hist = require('./functions/git.hist.js');
+var _checkout = require('./functions/git.checkout.js');
 
 module.exports = {
 	init: _init,
   getHistory: _hist,
 	status: _status,
 	add: _add,
-	commit: _commit
+	commit: _commit,
+  checkout: _checkout
 }

4519dee667ba6d5c6d0ce4a641c7f46fecaf153f
Greg Fedirko <gjfedirko@gmail.com>
2014-11-10 19:46:15 -0800

    git commit working

--- js/git/functions/git.commit.js
+++ js/git/functions/git.commit.js
@@ -3,11 +3,15 @@ var path = require('path');
 var fs = require('fs');
 
 module.exports = function(rootFilePath, message) {
+  var resolvedPath = path.resolve(rootFilePath);
 
-	var repo = gift(rootFilePath);
+  var repo = gift(resolvedPath);
 
-	repo.commit(message, {}, function(err) {
-		console.log(err);
-	});
+  repo.commit(message, {all: true, amend: false, author: 'greg fedirko'}, function(err) {
+    if (err) {
+      console.log(err);
+    }
+  })
+
 
 };
